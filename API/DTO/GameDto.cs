925bad8c1e7e6c2f05021615807f5a3ec596bf9a
Cazumy <maxim-cherkasov@mail.ru>
2025-04-15 14:38:04 +0300

    norm dto

--- API/DTO/GameDto.cs
+++ API/DTO/GameDto.cs
@@ -5,6 +5,6 @@ namespace API.DTO
     public record GameDto(
         [Required] int Id, 
         [Required] [Range(1, int.MaxValue)] int SteamId, 
-        [StringLength(300, MinimumLength = 5/*хз 78*/)] string? UserNote, 
+        [StringLength(300, MinimumLength = 5/*хз сколько*/)] string? UserNote, 
         int SaleToNotify);
 }
