bbf66357999744bf30658bbcf7ee70cc5ae1bc14
Nicetas <metchalatar@gmail.com>
2025-05-28 01:58:25 +0300

    фывфыв123

--- Steamviewer/Steamviewer/Entities/SteamModels/GameItem.cs
+++ Steamviewer/Steamviewer/Entities/SteamModels/GameItem.cs
@@ -21,57 +21,9 @@ namespace Steamviewer.Entities.SteamModels
         [JsonPropertyName("price")]
         public PriceInfo Price { get; set; }
 
-        // ===== поля с заглушками =====
-
-        /// <summary>
-        /// Теги игры (жанры, категории и т.п.), 
-        /// </summary>
-        [JsonPropertyName("tags")]
-        public List<string> Tags { get; set; } = new List<string> { "Открытый мир", "Автоматизация", "Песочница" };
-
-        /// <summary>
-        /// Описание игры 
-        /// </summary>
-        [JsonPropertyName("description")]
-        public string Description { get; set; } = "Описание игры пока отсутствует.";
-
-        /// <summary>
-        /// Дата выхода игры — желательно ISO-формат (yyyy-MM-dd).
-        /// </summary>
-        [JsonPropertyName("release_date")]
-        public DateTime? ReleaseDate { get; set; } = DateTime.Today;
-
-        /// <summary>
-        /// Разработчик игры 
-        /// </summary>
-        [JsonPropertyName("developer")]
-        public string Developer { get; set; } = "Неизвестный разработчик";
-
-        /// <summary>
-        /// Издатель игры 
-        /// </summary>
-        [JsonPropertyName("publisher")]
-        public string Publisher { get; set; } = "Неизвестный издатель";
-
-        /// <summary>
-        /// Уникальный идентификатор игры в Steam
-        /// </summary>
-        public int SteamID { get; set; }
-
-        /// <summary>
-        /// Заметка пользователя об игре
-        /// </summary>
-        public string? UserNote { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Скидка, при которой уведомлять (0–100)
-        /// </summary>
-        public int SaleToNotify { get; set; } = 0;
-
-        /// <summary>
-        /// Список вишлистов, к которым принадлежит игра
-        /// </summary>
-        //public List<Wishlist> Wishlists { get; set; } = new();
+        // Это класс, который полностью должен повторять JSON-запрос с стим апи. Тут не должно быть сторонней информации, поэтому
+        // я подвязал карточку игры к новому классу, который состоит из результата запроса JSON и нашим писаным бэкенд-классом.
+        // завтра (28.05) буду пороться чтобы всё было
     }
 
 }
